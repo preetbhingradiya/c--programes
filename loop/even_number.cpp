b376b2b900d72ba2a724abbb3e02d6509f31f9e6
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-07-06 10:56:04 +0530

    even_odd

---
+++ loop/even_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int no;
+    cout << "enter the number :";
+    cin >> no;
+
+    cout<<"Print even number:\n";
+    for (int i = 0; i <= no; i++)
+    {
+        if(i%2==0){
+        cout <<i<<"\n";
+         i++;
+        }
+    }
+
+    cout<<"Print odd number:\n";
+    for (int i = 0; i <= no; i++)
+    {
+        if(i%2==1){
+        cout <<i<<"\n";
+        i++;
+        }
+    }
+}
