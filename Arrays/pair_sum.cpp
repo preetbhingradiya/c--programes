c4c32ee94ad5094dfbada4775814af20f7e1d2b7
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-07-19 19:27:19 +0530

    pair_sum

---
+++ Arrays/pair_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // sorting order
+    int arr[7] = {1,2, 2, 4, 5, 6, 7};
+    int size = 7;
+    int sum = 5;
+    int ele1 = 0;
+    int ele2=0;
+
+    for (int i = 0; i < size; i++)
+    {
+
+        int element = arr[i];
+        for (int j = 1; j < size; j++)
+        {
+            int value = arr[j];
+            if (element + value == sum)
+            {
+                ele1=min(arr[i],arr[j]);
+                ele2=max(arr[i],arr[j]);
+                cout<<ele1<<" "<<ele2;
+            }
+        }
+    }
+}
