acd5faf275f747ed8fb8bf1917cecf749c0549f3
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-08-04 21:58:17 +0530

    move-zero

---
+++ Arrays/move-zero.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[6] = {0, 1, 3, 0, 1, 12};
+    int size = 6;
+
+    // for(int i=0;i<size;i++){
+    //     cout<<arr[i]<<" ";
+    // }cout<<endl;
+
+    int i = 0;
+    for (int j = 0; j < size; j++)
+    {
+        if (arr[j] == 0)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
