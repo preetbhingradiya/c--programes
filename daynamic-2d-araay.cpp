0ea2d3327e60e82f73ca5ac3b7c1ea679268b869
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-10-13 21:33:39 +0530

    relesing memory

---
+++ daynamic-2d-araay.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+
+    int **arr = new int *[row];
+    for (int i = 0; i < row; i++)
+    {
+        arr[i] = new int[col]; // print coloum in 2d arry
+    }
+
+    // taking input
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl;
+
+    // taking output
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // relsessing memory
+    for (int i = 0; i < row; i++)
+    {
+        delete[] arr[i];
+    }
+
+    delete []arr;
+}
