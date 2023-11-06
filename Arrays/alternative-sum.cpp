cee4c3d002fb7cb77e2f621202344435de13d5c0
preetbhingradiya <preetbhingradiya6@gmail.com>
2023-11-06 13:14:04 +0530

    add

--- Arrays/alternative-sum.cpp
+++ Arrays/alternative-sum.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 int findesum(int arr[], int n)
 {   
-    int i=0,store;
+    int i=0;
+    int store;
     while (i < n - 1)
     {
         int j = i + 2;
 
         if (j < n)
         {
-            store = arr[i] + arr[j];
+           store = arr[i] + arr[j];
+            return store;
         }
     }
-    return store;
 }
+
+
 
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout<<findesum(arr, n);
+    cout<<"value of : "<<findesum(arr, n);
 }
